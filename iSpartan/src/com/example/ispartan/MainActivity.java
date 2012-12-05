079c3658a212c405dde49061c8d938051f070159
Danny Ng <dannyng824@gmail.com>
2012-12-04 20:49:06 -0800

    Updates

--- iSpartan/src/com/example/ispartan/MainActivity.java
+++ iSpartan/src/com/example/ispartan/MainActivity.java
@@ -1,31 +1,48 @@
 package com.example.ispartan;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    
+    /** Called when the user clicks the Update button */
+    public void updateStatus(View view) {
+    	TextView status = (TextView) findViewById(R.id.statusTextView);
+    	status.setText("Updated!\nNextline");
     }
     
     /** Called when the user clicks the Make Recommendation button */
     public void makeRecommendation(View view) {
-    	Intent intent = new Intent(this, makeRecommendation.class);
+    	Intent intent = new Intent(this, MakeRecommendationActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void updateRecommendations() {
+    	Intent recIntent = getIntent();
+    	ArrayList<String> recArr = recIntent.getStringArrayListExtra("recommendations");
+    	String recommendations = "";
+    	for (String rec : recArr) {
+    		recommendations += rec;
+    	}
+    	EditText recView = (EditText) findViewById(R.id.recommendationEditText);
+    	recView.setText(recommendations);
+    }
+    
+    /** Called when the user clicks the Add Alert button */
+    public void makeAlert(View view) {
+    	Intent intent = new Intent(this, MakeAlertActivity.class);
     	startActivity(intent);
     }
     
